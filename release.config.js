6282e7c6d477fd3b81fa7556d808de0bd6b881b4
Fredrik Westmark <fredrik.westmark@gmail.com>
2020-01-30 13:13:38 +0100

    chore: update config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   extends: ["semantic-release-commit-filter"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      ("@semantic-release/git",
+      {
+        assets: ["packages/**/CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      })
+    ]
+  ],
   release: {
     branches: ["master"]
-  },
+  }
 };
